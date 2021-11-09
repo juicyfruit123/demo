42f1814de3a84c81707127ed6693e674f45a6fc0
a19458159 <AIgoreMakarov@sberbank.ru>
2021-11-09 16:19:42 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/service/ImageUploadService.java
@@ -0,0 +1,151 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Image;
+import com.example.demo.entity.Post;
+import com.example.demo.entity.User;
+import com.example.demo.exceptions.ImageNotFoundException;
+import com.example.demo.repository.ImageRepository;
+import com.example.demo.repository.PostRepository;
+import com.example.demo.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+
+@Service
+public class ImageUploadService {
+    public static final Logger LOG = LoggerFactory.getLogger(ImageUploadService.class);
+
+
+    private ImageRepository imageRepository;
+    private UserRepository userRepository;
+    private PostRepository postRepository;
+
+    @Autowired
+    public ImageUploadService(ImageRepository imageRepository, UserRepository userRepository, PostRepository postRepository) {
+        this.imageRepository = imageRepository;
+        this.userRepository = userRepository;
+        this.postRepository = postRepository;
+    }
+
+    public Image uploadImageTouser(MultipartFile file, Principal principal) throws IOException{
+        User user = getUserByPrincipal(principal);
+        LOG.info("Uploading image profile to User {}", user.getUsername());
+
+        Image userProfileImage = imageRepository.findByUserId(user.getId()).orElse(null);
+        if (!ObjectUtils.isEmpty(userProfileImage)){
+            imageRepository.delete(userProfileImage);
+        }
+
+        Image image = new Image();
+        image.setUserId(user.getId());
+        image.setImageBytes(compressBytes(file.getBytes()));
+        image.setName(file.getOriginalFilename());
+        return imageRepository.save(image);
+    }
+
+    public Image uploadImageToPost(MultipartFile file , Principal principal , Long postId) throws IOException{
+        User user = getUserByPrincipal(principal);
+        Post post = user.getPosts()
+                .stream()
+                .filter(post1 -> post1.getId().equals(postId))
+                .collect(toSinglePostCollector());
+
+        Image image = new Image();
+        image.setPostId(post.getId());
+        image.setImageBytes(file.getBytes());
+        image.setImageBytes(compressBytes(file.getBytes()));
+        image.setName(file.getOriginalFilename());
+        LOG.info("Uploading image to Post {}", post.getId());
+
+        return imageRepository.save(image);
+    }
+
+    public Image getImageToUser(Principal principal){
+        User user = getUserByPrincipal(principal);
+
+        Image image = imageRepository.findByUserId(user.getId()).orElse(null);
+        if (!ObjectUtils.isEmpty(image)){
+            image.setImageBytes(decompressBytes(image.getImageBytes()));
+        }
+
+        return image;
+    }
+
+    public Image    getImageToPost(Long postId){
+        Image image = imageRepository.findByPostId(postId)
+                .orElseThrow(()-> new ImageNotFoundException("Cannot find image to Post: " + postId));
+
+        if (!ObjectUtils.isEmpty(image)){
+            image.setImageBytes(decompressBytes(image.getImageBytes()));
+        }
+        return image;
+    }
+
+    private byte[] compressBytes(byte[] data) {
+        Deflater deflater = new Deflater();
+        deflater.setInput(data);
+        deflater.finish();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            LOG.error("Cannot compress Bytes");
+        }
+        System.out.println("Compressed Image Byte Size - " + outputStream.toByteArray().length);
+        return outputStream.toByteArray();
+    }
+
+    private static byte[] decompressBytes(byte[] data) {
+        Inflater inflater = new Inflater();
+        inflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        try {
+            while (!inflater.finished()) {
+                int count = inflater.inflate(buffer);
+                outputStream.write(buffer, 0, count);
+            }
+            outputStream.close();
+        } catch (IOException | DataFormatException e) {
+            LOG.error("Cannot decompress Bytes");
+        }
+        return outputStream.toByteArray();
+    }
+
+    private User getUserByPrincipal(Principal principal) {
+        String username = principal.getName();
+        return userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found with username " + username));
+
+    }
+
+    private <T> Collector<T,?,T> toSinglePostCollector(){
+        return Collectors.collectingAndThen(
+                Collectors.toList(),
+                list->{
+                    if (list.size()!=1){
+                        throw new IllegalStateException();
+                    }
+                    return list.get(0);
+                }
+        );
+    }
+}
